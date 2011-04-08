e215f3831339da925bb3ce4eebccb8370c625aaa
Darren Murray <dmurray@ccistudios.com>
2011-04-08 08:26:50 -0400

    initial site structure

---
+++ template/templates/mathany/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC 
+"-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" href="/templates/mathany/css/template.css" />
+	<title>Dr.Mathany</title>
+</head>
+
+<body>
+	<div id="top"><div>
+			<jdoc:include type="modules" name="top" style="xhtml"/>
+	</div></div>
+	
+	<div id="masthead"><div>
+		<jdoc:include type="modules" name="mid" style="xhtml"/>
+	</div></div>
+	
+	<div id="body"><div>
+		<div id="content"><div>
+			<div id="bottom"><div>
+				<jdoc:include type="modules" name="bottom" style="xhtml"/>
+			</div></div>
+			<div class="clear"></div>
+		</div></div>
+	</div></div>
+	
+
+	
+	<div id="footer"><div>
+		<div>&copy; Copyright <?php echo date('Y') ?> Dr. Drew Mathany &amp; Associates. All Rights Reserved.</div>
+			<div class="clear"></div>
+		 	 <span>Site by<a href="http://www.ccistudios.com" target="_blank"> CCI Studios</a></span>
+	</div></div>
+	
+</body>
+
+</html>
